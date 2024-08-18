e18151576b8394de6c78e6b7b24256d0bcf5270a
Jatin <jatinvashishtha053@gmail.com>
2024-08-18 21:55:48 +0530

    tranport routes and controller done but testing of apis remaing

--- src/routes/transport.route.js
+++ src/routes/transport.route.js
@@ -5,8 +5,8 @@ const router = Router();
 
 router.route("/create-transport").post(createTransport);
 router.route("/update-transport/:id").patch(updateTransport);
-router.route("/update-thumbnail/:id").patch(updateThumbnailTransport);
-router.route("/update-images/:id").patch(updateImagesTransport);
+router.route("/update-thumbnail/:id").patch(upload.single("tranport_thumbnail"),updateThumbnailTransport);
+router.route("/update-images/:id").patch(upload.array("transport_images",5),updateImagesTransport);
 router.route("/get-transport/:id").get(getTransportById);
 router.route("/get-transport").get(getAllTransport);
 router.route("/delete-transport/:id").delete(deleteTransport);
