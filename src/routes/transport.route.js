634fbbb9ff61c1dc8b715b7420ad33cb46c582eb
Jatin <jatinvashishtha053@gmail.com>
2024-08-18 22:03:42 +0530

    tranport route page small corection updated successfully

--- src/routes/transport.route.js
+++ src/routes/transport.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { createTransport, getTransportById, updateImagesTransport, updateThumbnailTransport, updateTransport, deleteTransport, getAllTransport } from "../controllers/transport.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
