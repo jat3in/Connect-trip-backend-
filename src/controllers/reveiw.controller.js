0c1eec8759093fdec1966087c92639771ca33b28
Jatin <jatinvashishtha053@gmail.com>
2024-08-20 19:11:24 +0530

    updated on reveiw controller page

--- src/controllers/reveiw.controller.js
+++ src/controllers/reveiw.controller.js
@@ -2,7 +2,6 @@ import { ReviewRatings } from "../models/reveiws.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponce } from "../utils/ApiResponce.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary, deleteOnCloudinary } from "../utils/cloudnairy.js";
 
 
 const createReview = asyncHandler( async (req,res) => {
