a2aa4b9ebd791ed1d873373649ae5cd6e2753ae6
Jatin <jatinvashishtha053@gmail.com>
2024-08-22 12:31:11 +0530

    testing and apis writting in readme file done of booking

--- src/routes/booking.route.js
+++ src/routes/booking.route.js
@@ -1,13 +1,14 @@
 import {Router} from "express";
 import {createBooking,updateBooking,getAllBooking,getBookingById,deleteBooking} from "../controllers/booking.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 
-router.route("/create-booking").post(createBooking);
-router.route("/update-booking/:id").patch(updateBooking);
-router.route("/get-booking").get(getAllBooking);
-router.route("/get-booking/:id").get(getBookingById);
-router.route("/delete-booking/:id").delete(deleteBooking);
+router.route("/create-booking").post(verifyJwt,createBooking);
+router.route("/update-booking/:id").patch(verifyJwt,updateBooking);
+router.route("/get-booking").get(verifyJwt,getAllBooking);
+router.route("/get-booking/:id").get(verifyJwt,getBookingById);
+router.route("/delete-booking/:id").delete(verifyJwt,deleteBooking);
 
 
 
