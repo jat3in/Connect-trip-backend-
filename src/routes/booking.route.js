2ec85c045bf4fd5d984eb8ff7538e1c873f7f090
Jatin <jatinvashishtha053@gmail.com>
2024-08-22 10:45:29 +0530

    booking routes setup successfully

--- src/routes/booking.route.js
+++ src/routes/booking.route.js
@@ -2,6 +2,12 @@ import {Router} from "express";
 const router = Router();
 
 
+router.route("/create-booking").post();
+router.route("/update-booking").patch();
+router.route("/get-booking").get();
+router.route("/get-booking/:id").get();
+router.route("/delete-booking/:id").delete();
+
 
 
 export default router;
