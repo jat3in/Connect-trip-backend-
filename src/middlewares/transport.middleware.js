2dc7925a5c268a6d18b63e63a4ed4280568f0bc2
Jatin <jatinvashishtha053@gmail.com>
2024-08-27 11:32:05 +0530

    transport middleware updated successfully

--- src/middlewares/transport.middleware.js
+++ src/middlewares/transport.middleware.js
@@ -4,6 +4,12 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const calculateRoute = asyncHandler( async (req,res) => {
     const {transport_from, transport_to} = req.body;
     console.log(transport_from, transport_to);
-    
-    
-})
+
+    const route = `${transport_from} - ${transport_to}`;
+    console.log(route);
+
+    req.route = route;
+    next();   
+});
+
+export {calculateRoute}
