0a5515f7fdf8168cfb01d5b1162e137b2cc7cb84
Jatin <jatinvashishtha053@gmail.com>
2024-08-27 11:38:58 +0530

    transport route updated

--- src/routes/transport.route.js
+++ src/routes/transport.route.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { createTransport, getTransportById, updateThumbnailTransport, updateTransport, deleteTransport, getAllTransport } from "../controllers/transport.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { calculateRoute } from "../middlewares/transport.middleware.js";
 
 const router = Router();
 
-router.route("/create-transport").post(verifyJwt,createTransport);
+router.route("/create-transport").post(verifyJwt,calculateRoute,createTransport);
 router.route("/update-transport/:id").patch(verifyJwt,updateTransport);
 router.route("/update-thumbnail/:id").patch(verifyJwt,upload.single("transport_thumbnail"),updateThumbnailTransport);
 router.route("/get-transport/:id").get(getTransportById);
