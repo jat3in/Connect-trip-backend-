784d720b875a1dce833b0f1b6cb0041ba589e2fd
Jatin <jatinvashishtha053@gmail.com>
2024-08-28 18:25:23 +0530

    all routes page updated successfully

--- src/app.js
+++ src/app.js
@@ -17,60 +17,60 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // tourist route import here..
-import touristRouter from "./routes/tourist.route.js";
+import touristRouter from "./routes/tourist.routes.js";
 
 app.use("/api/v1/tourist",touristRouter);
 
 // destination route import here...
-import destinationRouter from "./routes/destination.route.js";
+import destinationRouter from "./routes/destination.routes.js";
 
 app.use("/api/v1/destination", destinationRouter);
 
 // holidays routes comes here...
 
-import holidayRouter from "./routes/holiday.route.js";
+import holidayRouter from "./routes/holiday.routes.js";
 
 app.use("/api/v1/holiday", holidayRouter);
 
 // flights routes will goes here....
 
-import flightRouter from "./flights/flight.route.js";
+import flightRouter from "./flights/flight.routes.js";
 
 app.use("/api/v1/flight", flightRouter);
 
 // accomodation routes goes here....
 
-import accomodationRouter from "./routes/accomodation.route.js";
+import accomodationRouter from "./routes/accomodation.routes.js";
 
 app.use("/api/v1/accomodation", accomodationRouter);
 
 // activities routes goes here...
 
-import activitiesRouter from "./routes/activities.route.js";
+import activitiesRouter from "./routes/activities.routes.js";
 
 app.use("/api/v1/activity", activitiesRouter);
 
 // package routes goes here...
 
-import packageRouter from "./routes/package.route.js";
+import packageRouter from "./routes/package.routes.js";
 
 app.use("/api/v1/package", packageRouter);
 
 // transport routes goes here...
 
-import transportRouter from "./routes/transport.route.js";
+import transportRouter from "./routes/transport.routes.js";
 
 app.use("/api/v1/transport", transportRouter);
 
 // reveiew routes goes here... and completed with apis...
 
-import reveiwRouter from "./routes/reveiw.route.js";
+import reveiwRouter from "./routes/reveiw.routes.js";
 
 app.use("/api/v1/reveiw", reveiwRouter);
 
 // booking routes goes here....
 
-import bookingRouter from "./routes/booking.route.js";
+import bookingRouter from "./routes/booking.routes.js";
 
 app.use("/api/v1/booking", bookingRouter);
 
