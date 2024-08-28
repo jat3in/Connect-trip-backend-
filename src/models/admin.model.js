57a354f0f2d1bfb62632765255790fa2d0e7ca60
Jatin <jatinvashishtha053@gmail.com>
2024-08-28 18:11:37 +0530

    admin model completed successfully

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const adminSchema = Schema({
     fullName:{
@@ -46,6 +48,46 @@ const adminSchema = Schema({
     }
 
 },{timestamps: true});
+
+adminSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+    
+});
+
+adminSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+    
+};
+
+
+adminSchema.methods.generateAcessToken = function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        fullName: this.fullName,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: 86400,
+      }
+    );
+  };
+
+
+ adminSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: 864000,
+      }
+    );
+  };
 
 
 export const Admin = mongoose.model("admin",adminSchema);
