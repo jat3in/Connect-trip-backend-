5bb0e91b40b6f47ee758037f13ecf3641ae76783
Jatin <jatinvashishtha053@gmail.com>
2024-08-28 18:08:47 +0530

    admin routes added successfully

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -18,6 +18,11 @@ const loginAdmin = asyncHandler( async (req, res) => {
     return res.send("listning on the login admin route");
 });
 
+const updateAdmin = asyncHandler( async (req,res) => {
+
+    return res.send("listning on the update admin route");
+});
+
 const adminProfilePic = asyncHandler( async (req,res) => {
 
     return res.send("listning on the admin profile route");
@@ -40,6 +45,12 @@ const getAdminById = asyncHandler( async (req,res) => {
     return res.send("listning on the get admin by id");
 });
 
+const changeCurrentPassword = asyncHandler( async (req,res) => {
+
+
+    return res.send("listning on the change current password");
+});
+
 const deleteAdmin = asyncHandler( async (req,res) => {
 
 
@@ -47,4 +58,4 @@ const deleteAdmin = asyncHandler( async (req,res) => {
 });
 
 
-export { regiterAdmin, loginAdmin, getAdminById, getAllAdmin, getCurrentAdmin, deleteAdmin, adminProfilePic}
+export { regiterAdmin, loginAdmin, getAdminById, getAllAdmin, getCurrentAdmin, deleteAdmin, adminProfilePic, updateAdmin, changeCurrentPassword}
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
-import { regiterAdmin, loginAdmin, getAdminById, getAllAdmin, getCurrentAdmin, deleteAdmin, adminProfilePic} from "../controllers/admin.controller.js";
+import { regiterAdmin, loginAdmin, getAdminById, getAllAdmin, getCurrentAdmin, deleteAdmin, adminProfilePic, updateAdmin, changeCurrentPassword} from "../controllers/admin.controller.js";
 const router = Router();
 
+router.route("/register").post(regiterAdmin);
+router.route("/login").post(loginAdmin);
+router.route("/update/admin/:id").patch(updateAdmin);
+router.route("/change-password/:id").patch(changeCurrentPassword);
+router.route("/admins").get(getAllAdmin);
+router.route("/admin/:id").get(getAdminById);
+router.route("/current/admin").get(getCurrentAdmin);
+router.route("/delete/admin/:id").delete(deleteAdmin);
+router.route("/admin/profile-pic/:id").patch(adminProfilePic);
 
 
 export default router;
